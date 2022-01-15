1794a9e28b60a9ec4450c4e0e84c3ba656a156d0
Md. Siam <md.siam03@gmail.com>
2022-01-15 15:26:07 +0600

    syncfusion_charts

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class MyApp extends StatelessWidget {
       //home: const MyFlutterTTS(),
       //home: const MyFlutterHighlight(),
       home: const MySyncfusionFlutterCharts(),
+     
     );
   }
 }
