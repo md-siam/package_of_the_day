62eac95042dc77ba78004c66cc0a94da66bcb03c
Md. Siam <md.siam03@gmail.com>
2022-04-29 02:53:15 +0600

    get_it

---
+++ lib/59_get_it/get_it.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyGetIt extends StatelessWidget {
+  const MyGetIt({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Get It')),
+    );
+  }
+}
