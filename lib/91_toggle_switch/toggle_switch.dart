d378021acd387cbaf798420d8c1b945c6319eaa1
md-siam <md.siam03@gmail.com>
2022-08-13 19:29:17 +0600

    fix: minor fixes

--- lib/91_toggle_switch/toggle_switch.dart
+++ lib/91_toggle_switch/toggle_switch.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: deprecated_member_use
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:toggle_switch/toggle_switch.dart';
